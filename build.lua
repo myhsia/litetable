9582240551a7e46c190a94bdd953c0d2dde31074
Mingyu Xia <myhsia@outlook.com>
2025-03-12 18:06:34 +0800

    Version 3.3E released.     - Updated the manual and l3build workflow.

---
+++ build.lua
@@ -0,0 +1,37 @@
+--[==========================[--
+   L3BUILD FILE FOR LITETABLE
+--]==========================]--
+
+module       = "litetable"
+ctanzip      = module
+typesetfiles = {"*.dtx", "*-zh-cn.tex", "*-zh-hk.tex"}
+specialtypesetting = specialtypesetting or {}
+specialtypesetting["litetable-zh-cn.tex"] =
+  {cmd = "xelatex --shell-escape -interaction=nonstopmode"}
+specialtypesetting["litetable-zh-hk.tex"] =
+  {cmd = "xelatex --shell-escape -interaction=nonstopmode"}
+
+uploadconfig = {
+  pkg          = "litetable",
+  version      = "v3.3E 2025-03-12",
+  author       = "Mingyu Xia; Lijun Guo",
+  uploader     = "Mingyu Xia",
+  email        = "myhsia@outlook.com",
+  summary      = "Class schedules with colorful course blocks",
+  description  = [[
+    The litetable class provides a colorful timetable design,
+    developed by expl3 based on article and TikZ
+  ]],
+  license      = "lppl1.3c",  
+  ctanPath     = "/macros/latex/contrib/litetable",
+  announcement = [[
+    Version 3.3E released.
+    - Updated the manual and l3build workflow.
+  ]],
+  home         = "https://github.com/myhsia/litetable",
+  bugtracker   = "https://github.com/myhsia/litetable/issues",
+  support      = "https://qm.qq.com/q/RGFmHwBecC",
+  repository   = "https://github.com/myhsia/litetable",
+  development  = "https://github.com/myhsia",
+  update       = true
+}
