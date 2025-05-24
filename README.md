aa10dab97a6d24168821e6195c1d931b0f3aca83
Mingyu Xia <myhsia@outlook.com>
2025-05-24 18:43:30 +0800

    v3.3I Released.

---
+++ README.md
@@ -0,0 +1,38 @@
+[![CTAN Version](https://img.shields.io/ctan/v/litetable)](https://ctan.org/pkg/litetable)
+[![GitHub Release](https://img.shields.io/github/v/release/myhsia/litetable)](https://github.com/myhsia/litetable/releases/latest)
+[![Actions Status](https://github.com/myhsia/litetable/actions/workflows/main.yaml/badge.svg?branch=main)](https://github.com/myhsia/litetable/actions)
+[![GitHub Repo stars](https://img.shields.io/github/stars/myhsia/litetable)](https://github.com/myhsia/litetable)
+[![GitHub Last Commit](https://img.shields.io/github/last-commit/myhsia/litetable)](https://github.com/myhsia/litetable/commits)
+
+# The `litetable` Package
+
+## Overview
+
+The `litetable` package (conducted with LaTeX3) provides a colorful design
+of timetable. See `litetable.pdf` for more. Happy TeXing!
+
+## Issues
+
+The issue tracker for `litetable` is currently located
+[on GitHub](https://github.com/myhsia/litetable/issues).
+
+## Build status
+
+This project uses [GitHub Actions](https://github.com/features/actions)
+as a hosted continuous integration service. For each commit, the build status
+is tested using the current release of TeX Live.
+
+_Current build status:_ ![build status](https://github.com/myhsia/litetable/actions/workflows/main.yaml/badge.svg?branch=main)
+
+## Copyright and License
+
+  Copyright (C) 2023-2025 by Mingyu Xia <myhsia@outlook.com>
+
+  It may be distributed and/or modified under the conditions of the
+  LaTeX Project Public License (LPPL), either version 1.3c of this
+  license or (at your option) any later version. The latest version
+  of this license is in the file <http://www.latex-project.org/lppl.txt>
+
+  This work has the LPPL maintenance status `maintained`.
+
+  The Current Maintainer of this work is **Mingyu Xia**.
--- build.lua
+++ build.lua
@@ -1,27 +1,28 @@
 --[==========================================[--
            L3BUILD FILE FOR LITETABLE
-     Check PDF File & Directory After Build
+      Once Pushed After This File Modified
+        A New Release Will Be Published
 --]==========================================]--
 
 --[==========================================[--
-                Basic Information
-             Do Check Before Upload
+               Basic Information
+             Do Check Before Push
 --]==========================================]--
 
 module       = "litetable"
-version      = "v3.3H 2025-05-23"
+version      = "v3.3I 2025-05-24"
 maintainer   = "Mingyu Xia; Lijun Guo"
 uploader     = "Mingyu Xia"
 maintainid   = "myhsia"
 email        = "myhsia@outlook.com"
 repository   = "https://github.com/" .. maintainid .. "/" .. module
 announcement = [[
-  Version 3.3H released.
-  - Optimized the implementation
+Version 3.3I released.
+- `build.lua` is included in the package.
 ]]
 summary      = "Class schedules with colorful course blocks"
 description  = [[
-The litetable package provides a colorful timetable design, developed by expl3 based on article and TikZ
+The litetable package provides a colorful timetable design, developed by expl3 based on TikZ
 ]]
 
 --[==========================================[--
@@ -29,12 +30,14 @@ The litetable package provides a colorful timetable design, developed by expl3 b
          Don't Modify Unless Necessary
 --]==========================================]--
 typesetfiles = {"*.dtx", "*-zh-cn.tex", "*-zh-hk.tex"}
-typesetsourcefiles  = {"*.md"}
 specialtypesetting = specialtypesetting or {}
 specialtypesetting["litetable-zh-cn.tex"] =
   {cmd = "xelatex --shell-escape -interaction=nonstopmode"}
 specialtypesetting["litetable-zh-hk.tex"] =
   {cmd = "xelatex --shell-escape -interaction=nonstopmode"}
+textfiles    = {"*.md", "LICENSE", "*.lua"}
+excludefiles = {"*~"}
+cleanfiles   = {"*.log", "*.pdf", "*.zip", "*.curlopt"}
 ctanzip      = module
 
 uploadconfig = {
@@ -55,3 +58,22 @@ uploadconfig = {
   development  = "https://github.com/" .. maintainid,
   update       = true
 }
+
+function unpack(sources, sourcedirs)
+  local errorlevel = dep_install(unpackdeps)
+  if errorlevel ~= 0 then
+    return errorlevel
+  end
+  errorlevel = bundleunpack(sourcedirs, sources)
+  if errorlevel ~= 0 then
+    return errorlevel
+  end
+  for _,i in ipairs(installfiles) do
+    errorlevel = cp(i, unpackdir, localdir)
+    if errorlevel ~= 0 then
+      return errorlevel
+    end
+  end
+  cp("*.md", unpackdir, currentdir)
+  return 0
+end
