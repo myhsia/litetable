3126c4c287954e8bd771670cbc3acdd181d7d613
myhsia <myhsia@outlook.com>
2025-07-17 15:41:15 +0800

    Version 3.4C released.

--- build.lua
+++ build.lua
@@ -10,18 +10,17 @@
 --]==========================================]--
 
 module        = "litetable"
-version       = "v3.4B"
-date          = "2025-07-16"
+version       = "v3.4C"
+date          = "2025-07-17"
 maintainer    = "Mingyu Xia; Lijun Guo"
 uploader      = "Mingyu Xia"
 maintainid    = "myhsia"
 email         = "myhsia@outlook.com"
 repository    = "https://github.com/" .. maintainid .. "/" .. module
-announcement  = "Version " .. version .. " released.\n" .. [[
-- Fix the bug that `build.lua` script works improperly on Windows OS.]]
+announcement  = [[Version 3.4C released.
+- Enhanced the performance of `build.lua`.]]
 summary       = "A Colorful Timetable Design"
-description   = [[
-The litetable package provides a colorful timetable design, developed by expl3 based on TikZ]]
+description   = "The litetable package provides a colorful timetable design, developed by expl3 based on TikZ"
 
 --[==========================================[--
           Build, Pack, Tag, and Upload
@@ -31,17 +30,15 @@ The litetable package provides a colorful timetable design, developed by expl3 b
 ctanzip       = module
 cleanfiles    = {"*log", "*.pdf", "*.zip", "*.curlopt"}
 excludefiles  = {"*~"}
-latexmk       = "$makeindex = \"makeindex -s gind.ist\";"
+tagfiles      = {"*.dtx", "*.tex"}
 textfiles     = {"*.md", "LICENSE", "*.lua"}
 typesetcmds   = "\\AtBeginDocument\\DisableImplementation"
 typesetexe    = "latexmk -pdf"
 typesetfiles  = {"*.dtx", "*.tex"}
-typesetruns   = 1
 specialtypesetting = specialtypesetting or {}
 specialtypesetting["litetable-zh-cn.tex"] = {cmd = "latexmk -xelatex"}
 specialtypesetting["litetable-zh-hk.tex"] = {cmd = "latexmk -xelatex"}
-
-uploadconfig = {
+uploadconfig  = {
   pkg          = module,
   version      = version .. " " .. date,
   author       = maintainer,
@@ -59,8 +56,6 @@ uploadconfig = {
   development  = "https://github.com/" .. maintainid,
   update       = true
 }
-
-tagfiles = {"*.dtx", "*.tex"}
 function update_tag(file, content, tagname, tagdate)
   tagname = version
   tagdate = date
@@ -79,12 +74,6 @@ end
 
 function docinit_hook()
   cp("*.md", unpackdir, currentdir)
-  if os_type == "windows" then
-    patch_typeset = "echo " .. latexmk .. " > latexmkrc"
-  else
-    patch_typeset = "echo '" .. latexmk .. "' > latexmkrc"
-  end
-  run(typesetdir, patch_typeset)
   for _,i in ipairs(installfiles) do
     errorlevel = cp(i, unpackdir, typesetdir)
   end
@@ -93,5 +82,9 @@ end
 function tex(file,dir,cmd)
   dir = dir or "."
   cmd = cmd or typesetexe
-  return run(dir, cmd .. " " .. typesetopts .. " -usepretex='" .. typesetcmds .. "' " .. file)
+  if os_type == "windows" then
+    return run(dir, cmd .. " -usepretex=\"" .. typesetcmds .. "' -e '$makeindex = q/makeindex -s " .. indexstyle .. " %O %S/\" " .. file)
+  else
+    return run(dir, cmd .. " -usepretex='" .. typesetcmds .. "' -e '$makeindex = q/makeindex -s " .. indexstyle .. " %O %S/' " .. file)
+  end
 end
