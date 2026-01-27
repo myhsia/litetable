11f16338b19c56adcb8fcc5ec5e64cceb778aab0
myhsia <myhsia@outlook.com>
2026-01-27 15:27:55 +0800

    Updated the doc and copyright info in preambles

--- build.lua
+++ build.lua
@@ -10,14 +10,13 @@
 --]==========================================]--
 
 module              = "litetable"
-version             = "v3.8C"
-date                = "2025-11-15"
+version             = "v3.9A"
+date                = "2026-01-31"
 maintainer          = "Mingyu Xia; Lijun Guo"
 uploader            = "Mingyu Xia"
 maintainid          = "myhsia"
 email               = "myhsia@outlook.com"
 repository          = "https://github.com/" .. maintainid .. "/" .. module
-announcement        = ""
 summary             = "A Colorful Timetable Design"
 description         = "The litetable package provides a colorful timetable design, developed by expl3 based on TikZ"
 
@@ -40,22 +39,23 @@ specialtypesetting  = specialtypesetting or {}
 specialtypesetting["litetable-zh-cn.tex"] = {cmd = "latexmk -xelatex"}
 specialtypesetting["litetable-zh-hk.tex"] = {cmd = "latexmk -xelatex"}
 uploadconfig  = {
-  pkg          = module,
-  version      = version .. " " .. date,
-  author       = maintainer,
-  uploader     = uploader,
-  email        = email,
-  summary      = summary,
-  description  = description,
-  license      = "lppl1.3c",  
-  ctanPath     = "/macros/latex/contrib/" .. module,
-  announcement = announcement,
-  home         = "https://github.com/" .. maintainid,
-  bugtracker   = repository .. "/issues",
-  support      = repository .. "/issues",
-  repository   = repository,
-  development  = "https://github.com/" .. maintainid,
-  update       = true
+  announcement_file = "announcement.md",
+  pkg               = module,
+  version           = version .. " " .. date,
+  author            = maintainer,
+  uploader          = uploader,
+  email             = email,
+  summary           = summary,
+  description       = description,
+  license           = "lppl1.3c",  
+  ctanPath          = "/macros/latex/contrib/" .. module,
+  announcement      = announcement,
+  home              = "https://github.com/" .. maintainid,
+  bugtracker        = repository .. "/issues",
+  support           = repository .. "/issues",
+  repository        = repository,
+  development       = "https://github.com/" .. maintainid,
+  update            = true
 }
 function update_tag(file, content, tagname, tagdate)
   tagname = version
